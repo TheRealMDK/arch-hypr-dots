78ae1fcbc2e41b3b019ee50564b810ecec9ee7e6
Clinton Oliver <clintonoliver25@gmail.com>
2025-05-17 17:28:04 +0200

    update binds and aliases

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+- eog (pacman)
+- chrome
+- termusic
--- home/user/feather_cava_tmux.sh
+++ home/user/feather_cava_tmux.sh
@@ -18,10 +18,21 @@ tmux split-window -v -t "$SESSION"
 
 sleep 0.3
 
+# Focus on the bottom pane after setup
+tmux select-pane -t "$BOTTOM_LEFT_PANE"
+
+sleep 0.3
+
+# Split the window horizontally (left and right)
+tmux split-window -h -t "$SESSION"
+
+sleep 0.3
+
 # Get both updated pane IDs
 PANE_IDS=($(tmux list-panes -t "$SESSION" -F "#{pane_id}"))
 TOP_PANE="${PANE_IDS[0]}"
-BOTTOM_PANE="${PANE_IDS[1]}"
+BOTTOM_LEFT_PANE="${PANE_IDS[1]}"
+BOTTOM_RIGHT_PANE="${PANE_IDS[2]}"
 
 # Calculate 80% of the current terminal height
 # Get total lines (height) of the tmux window
@@ -39,12 +50,19 @@ tmux send-keys -t "$TOP_PANE" '/home/clinton/arch-hypr-dots/home/user/.config/Fe
 sleep 0.3
 
 # Run 'cava' in the bottom pane
-tmux send-keys -t "$BOTTOM_PANE" 'cava' C-m
+tmux send-keys -t "$BOTTOM_LEFT_PANE" 'cava' C-m
+
+sleep 0.3
+
+# Run 'pulsemixer' in the bottom right pane
+tmux send-keys -t "$BOTTOM_RIGHT_PANE" 'pulsemixer' C-m
 
 sleep 0.3
 
 # Focus on the top pane after setup
 tmux select-pane -t "$TOP_PANE"
+
+sleep 0.3
 
 # Attach to the session
 tmux attach-session -t "$SESSION"
